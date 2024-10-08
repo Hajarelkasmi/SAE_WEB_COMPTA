6ed617a572a7b418553a33db08acaeecc4856a40
Juliann MERIT <juliann.merit@gmail.com>
2024-10-08 15:29:39 +0200

    Ajout de categorie, page, classe_categorie et classe_page

---
+++ src/model/module/classe_page.js
@@ -0,0 +1,40 @@
+// classe_page.js
+
+const {Classe_Page} = require('../bd');
+
+module.exports = (app) => {
+    app.get('/api/classe_pages', async (req, res) => {
+        try {
+            const classe_pages = await Classe_Page.findAll();
+            res.json(classe_pages);
+        } catch (error) {
+            res.status(500).json({error: 'An error occurred while fetching classe_pages'});
+        }
+    });
+
+    app.post('/api/classe_pages', async (req, res) => {
+        try {
+            const classe_page = await Classe_Page.create({
+                classe_id: req.body.classe_id,
+                page_id: req.body.page_id
+            });
+            res.json(classe_page);
+        } catch (error) {
+            res.status(500).json({error: 'An error occurred while creating classe_page'});
+        }
+    });
+
+    app.delete('/api/classe_pages/:id_classe/:id_page', async (req, res) => {
+        try {
+            const classe_page = await Classe_Page.findByPk(req.params.id_classe, req.params.id_page);
+            if (classe_page) {
+                await classe_page.destroy();
+                res.json(classe_page);
+            } else {
+                res.status(404).json({error: 'Classe_Page not found'});
+            }
+        } catch (error) {
+            res.status(500).json({error: 'An error occurred while deleting classe_page'});
+        }
+    });
+}
