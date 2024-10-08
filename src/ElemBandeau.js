bf9869ab505d8e7e55c9974bf7e355622b650e49
Rousselet Juliette <juliette.rousselet@etu.univ-orleans.fr>
2024-10-08 18:15:15 +0200

    Refactor CSS and add navigation bar component

--- src/ElemBandeau.js
+++ src/ElemBandeau.js
@@ -2,6 +2,11 @@ function ElemBandeau({link, text, enfants=[]}) {
     return (
     <li>
         <a href={link}>{text}</a>
+        <ul>
+            {enfants.map((enfant, index) => (
+                <ElemBandeau key={index} link={enfant.link} text={enfant.text} enfants={enfant.enfants} />
+            ))}
+        </ul>
     </li>
     );
 }
