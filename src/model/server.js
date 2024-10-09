f2b77de23a5881174b218859e26db72e76dd109f
Baptiste Haudebourg <baptiste.haudebourg@etu.univ-orleans.fr>
2024-10-09 16:07:28 +0200

    ajout des routes au serveur

--- src/model/server.js
+++ src/model/server.js
@@ -1,7 +1,17 @@
 // server.js
 const express = require('express');
 const cors = require('cors');
-const configureClasseRoutes = require('./module/classe'); // Chemin correct vers le module classe.js
+const configureClasseRoutes = require('./module/classe'); 
+const configureClasseCategorieRoutes = require('./module/classe_categorie');
+const configureClassePageRoutes = require('./module/classe_page');
+const configureEtudiantRoutes = require('./module/etudiant'); 
+const configureLienRoutes = require('./module/lien'); 
+const configureArticleRoutes = require('./module/article'); 
+const configureCategorieRoutes = require('./module/categorie'); 
+const configurePageRoutes = require('./module/page');
+const configureVideoRoutes = require('./module/video');
+const configureExerciceRoutes = require('./module/exercice');
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,6 +20,15 @@ app.use(cors());
 app.use(express.json());
 
 configureClasseRoutes(app);
+configureClasseCategorieRoutes(app);
+configureClassePageRoutes(app);
+configureEtudiantRoutes(app);
+configureLienRoutes(app);
+configureArticleRoutes(app);
+configureCategorieRoutes(app);
+configurePageRoutes(app);
+configureVideoRoutes(app);
+configureExerciceRoutes(app);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
