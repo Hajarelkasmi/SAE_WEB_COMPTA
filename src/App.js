215a1a2404046e79a482be3e82b36763a8fce031
Rousselet Juliette <juliette.rousselet@etu.univ-orleans.fr>
2024-10-09 17:46:33 +0200

    elements du bandeau définis dans le bandeau maintenant définis dans l'appli puis passés au beandeau

--- src/App.js
+++ src/App.js
@@ -3,9 +3,35 @@ import './App.css';
 import Bandeau from './Bandeau';
 
 function App() {
+  let elems = [
+    {link: "/", text: "Accueil"},
+    {link: "/", text: "Comptabilité", enfants: [
+      {link: "/", text: "lien 1"},
+      {link: "/", text: "lien 2"},
+      {link: "/", text: "lien 3"},
+    ]},
+    {link: "/", text: "Contrôle de gestion", enfants: [
+      {link: "/", text: "lien a"},
+      {link: "/", text: "lien b"},
+      {link: "/", text: "lien c"},
+    ]},
+    {link: "/", text: "Gestion financière", enfants: [
+      {link: "/", text: "lien i"},
+      {link: "/", text: "lien ii"},
+      {link: "/", text: "lien iii"},
+    ]},
+    {link: "/", text: "Fiscalité financière"},
+    {link: "/", text: "Communication"},
+    {link: "/", text: "Pédagogie", enfants: [
+      {link: "/", text: "lien I"},
+      {link: "/", text: "lien II"},
+      {link: "/", text: "lien III"},
+    ]},
+    {link: "/", text: "Blog"},
+  ];
   return (
     <div className="App">
-      <Bandeau />
+      <Bandeau elems={elems} />
       <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </div>
--- src/Bandeau.js
+++ src/Bandeau.js
@@ -1,20 +1,15 @@
 import './Bandeau.css';
 import ElemBandeau from './ElemBandeau';
 
-function Bandeau() {
+function Bandeau({elems}) {
   return (
     <header>
         <nav>
             <a href='/' id="logohome"><img src="logo_bitmoji.png" alt="logo" class='logo' /></a>
             <ul id="pages">
-                <ElemBandeau link="/" text="Accueil" />
-                <ElemBandeau link="/" text="Comptabilité" enfants={[{link:"/", text:"lien 1"}, {link:"/", text:"lien 2"}, {link:"/", text:"lien 3"}]} />
-                <ElemBandeau link="/" text="Contrôle de gestion" enfants={[{link:"/", text:"lien a"}, {link:"/", text:"lien b"}, {link:"/", text:"lien c"}]} />
-                <ElemBandeau link="/" text="Gestion financière" enfants={[{link:"/", text:"lien i"}, {link:"/", text:"lien ii"}, {link:"/", text:"lien iii"}]} />
-                <ElemBandeau link="/" text="Fiscalité financière" />
-                <ElemBandeau link="/" text="Communication" />
-                <ElemBandeau link="/" text="Pédagogie" enfants={[{link:"/", text:"lien I"}, {link:"/", text:"lien II"}, {link:"/", text:"lien III"}]} />
-                <ElemBandeau link="/" text="Blog" />
+                {elems.map((elem, index) => (
+                    <ElemBandeau key={index} link={elem.link} text={elem.text} enfants={elem.enfants} />
+                ))}
             </ul>
             <ul id="reseaux">
               <li><a href='/'><img src="logo192.png" alt="logo" class='logo' /></a></li>
