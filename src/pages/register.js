32dd5322284b18f643300cdb09a35ddec5dd8ea1
amelie_brizard <amelie.brizard@etu.univ-orleans.fr>
2024-10-15 15:27:56 +0200

    Changement du css et ajout de nouveaux champs pour le formulaire

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './register.css'
 
 function register() {
     return (
@@ -9,19 +10,33 @@ function register() {
                 <h2>Inscription</h2>
                 <p>Inscrivez-vous pour accéder à tous nos cours et à une multitude d'exercices</p>
                 <form>
-                    <div class="register-form">
-                        <label for="email">Adresse mail</label>
-                        <input type="email" name="email" id="email" required />
+                    <div class="name-block">
+                        <div class="register-form">
+                            {/* <label for="nom">Nom</label> */}
+                            <input type="text" name="nom" id="nom" placeholder="Nom" required />
+                        </div>
+                        <div class="register-form">
+                            {/* <label for="prenom">Prénom</label> */}
+                            <input type="text" name="prenom" id="prenom" placeholder="Prénom" required />
+                        </div>
                     </div>
                     <div class="register-form">
-                        <label for="classe">Votre classe</label>
-                        <input type="text" name="classe" id="classe" required />
+                        {/* <label for="email">Adresse mail</label> */}
+                        <input type="email" name="email" id="email" placeholder="Adresse mail" required />
                     </div>
                     <div class="register-form">
-                        <label for="password">Mot de passe</label>
-                        <input type="password" name="password" id="password" required />
+                        {/* <label for="classe">Votre classe</label> */}
+                        <input type="text" name="classe" id="classe" placeholder="Votre classe" required />
                     </div>
-                    <input type="submit" value="Demander l'accès" />
+                    <div class="register-form">
+                        {/* <label for="password">Mot de passe</label> */}
+                        <input type="password" name="password" id="password" placeholder="Mot de passe" required />
+                    </div>
+                    <div>
+                        <input type="checkbox" name="access" id="access" />
+                        <label for="access">Demander l'accès/s'abonner.</label>
+                    </div>
+                    <input type="submit" value="S'inscrire" />
                 </form>
                 <p>Vous receverez un mail lorsque l'administrateur aura accepté votre demande.</p>
             </div>
@@ -30,3 +45,6 @@ function register() {
 }
 
 export default register;
+
+// nom prénom à rajouter
+// case à cocher "demander l'accès"
