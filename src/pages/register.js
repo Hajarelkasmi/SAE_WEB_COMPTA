02748e3d25a9890f9c9106551e68f2ebe54c735f
amelie_brizard <amelie.brizard@etu.univ-orleans.fr>
2024-10-21 11:42:03 +0200

    Amélioration du css, de la structure html de la page et ajout du logo

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,42 +1,28 @@
 import React from "react";
-import './register.css'
+import '../css/register.css'
+import logo from '../img/logo.png'
 
 function register() {
     return (
         <div>
-            <img></img>
+            <img src={logo} alt="logo of the website" id="register-logo" />
             <div class="register-main-block">
                 <h1>Bienvenue !</h1>
                 <h2>Inscription</h2>
-                <p>Inscrivez-vous pour accéder à tous nos cours et à une multitude d'exercices</p>
+                <p>Inscrivez-vous pour accéder à tous nos cours et à une multitude d'exercices.</p>
                 <form>
                     <div class="name-block">
-                        <div class="register-form">
-                            {/* <label for="nom">Nom</label> */}
-                            <input type="text" name="nom" id="nom" placeholder="Nom" required />
-                        </div>
-                        <div class="register-form">
-                            {/* <label for="prenom">Prénom</label> */}
-                            <input type="text" name="prenom" id="prenom" placeholder="Prénom" required />
-                        </div>
+                        <input type="text" name="nom" id="nom" placeholder="Nom" required />
+                        <input type="text" name="prenom" id="prenom" placeholder="Prénom" required />
                     </div>
-                    <div class="register-form">
-                        {/* <label for="email">Adresse mail</label> */}
-                        <input type="email" name="email" id="email" placeholder="Adresse mail" required />
-                    </div>
-                    <div class="register-form">
-                        {/* <label for="classe">Votre classe</label> */}
-                        <input type="text" name="classe" id="classe" placeholder="Votre classe" required />
-                    </div>
-                    <div class="register-form">
-                        {/* <label for="password">Mot de passe</label> */}
-                        <input type="password" name="password" id="password" placeholder="Mot de passe" required />
-                    </div>
-                    <div>
+                    <input type="email" name="email" id="email" placeholder="Adresse mail" required />
+                    <input type="text" name="classe" id="classe" placeholder="Votre classe" required />
+                    <input type="password" name="password" id="password" placeholder="Mot de passe" required />
+                    <div id="checkbox-register">
                         <input type="checkbox" name="access" id="access" />
                         <label for="access">Demander l'accès/s'abonner.</label>
                     </div>
-                    <input type="submit" value="S'inscrire" />
+                    <input id="register-button" type="submit" value="S'inscrire" />
                 </form>
                 <p>Vous receverez un mail lorsque l'administrateur aura accepté votre demande.</p>
             </div>
@@ -45,6 +31,3 @@ function register() {
 }
 
 export default register;
-
-// nom prénom à rajouter
-// case à cocher "demander l'accès"
