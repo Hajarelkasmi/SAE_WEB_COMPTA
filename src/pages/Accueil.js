8396d893d646e925bc454678540275c9cda42274
Rousselet Juliette <juliette.rousselet@etu.univ-orleans.fr>
2024-10-22 15:38:16 +0200

    Refactor Accueil page styling and responsiveness

--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -13,7 +13,12 @@ function Accueil() {
                     <div id="paragraphes">
                         <p>Enseignante en gestion comptable et financière 📈​ depuis plus de 20 ans, je vous propose de vous partager mes cours.</p>
                         <p>Je vous partage mes conseils pour que vous deveniez des étudiants aguerris.</p>
-                        <p>Ce site est encore en construction, alors pour retrouver dès maintenant toutes mes vidéos de cours de gestion comptable et financière.</p>
+                        <p>Ce site est encore en construction, alors vous pouvez retrouver dès maintenant toutes mes vidéos de cours de gestion comptable et financière sur Youtube.</p>
+                        {/* <p>Bonjour 👋​ et bienvenue sur mon site internet.</p>
+                        <p>👩‍🏫​ Enseignante  en gestion comptable et financière 📈​ depuis plus de 20 ans, je vous propose de vous partager mes cours.</p>
+                        <p>Parce que vous avez été absent, vous avez eu du mal à comprendre un cours, vous souhaitez approfondir une notion ou vous exercez en vue de vos futurs examens, je vous partage mes cours sous format de vidéos ou de fiches.</p>
+                        <p>De plus, vous avez peut-être des difficultés à apprendre, à rester motiver, à communiquer en respectant les codes des études supérieures, alors je vous partage mes conseils pour que vous deveniez des étudiants aguerris.</p>
+                        <p>Ce site est encore en construction 🛠️​, alors pour retrouver dès maintenant toutes mes vidéos de cours de gestion comptable et financière, vous pouvez les retrouver  directement sur ma chaine Youtube ▶️​.</p> */}
                     </div>
                 </div>
             </section>
