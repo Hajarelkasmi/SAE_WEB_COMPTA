8d41580a1414e2f1800fc5e805dfca1e49454541
Rousselet Juliette <juliette.rousselet@etu.univ-orleans.fr>
2024-10-22 12:23:16 +0200

    Refactor Accueil page to use JSX syntax and add styling

--- src/pages/Accueil.js
+++ src/pages/Accueil.js
@@ -1,9 +1,23 @@
+import "../styles/Accueil.css";
+
 function Accueil() {
     return (
-        <div>
-            <h1>Accueil</h1>
-            <p>Bienvenue sur notre site !</p>
-        </div>
+        <main>
+            <section id="titre">
+                <h1>Réussir ses études en gestion comptable et financière</h1>
+            </section>
+            <section id="presentation">
+                <img src="/logo_bitmoji.png" alt="Accueil" />
+                <div id="texte">
+                    <h2>La Comptabilité financière pour tous !</h2>
+                    <div id="paragraphes">
+                        <p>Enseignante en gestion comptable et financière 📈​ depuis plus de 20 ans, je vous propose de vous partager mes cours.</p>
+                        <p>Je vous partage mes conseils pour que vous deveniez des étudiants aguerris.</p>
+                        <p>Ce site est encore en construction, alors pour retrouver dès maintenant toutes mes vidéos de cours de gestion comptable et financière.</p>
+                    </div>
+                </div>
+            </section>
+        </main>
     );
 }
 
