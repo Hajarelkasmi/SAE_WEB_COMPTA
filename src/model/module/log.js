d7bb96d85e7f7d6a0d1defeb727a171987e397e0
Baptiste Haudebourg <baptiste.haudebourg@etu.univ-orleans.fr>
2024-10-24 09:11:56 +0200

    envoie des classes à la place des étudiants logs.js

--- src/model/module/log.js
+++ src/model/module/log.js
@@ -1,11 +1,26 @@
 const { verifyToken, verifyAdmin } = require('../auth');
-const { Connexion_Log } = require('../bd');
+const { Connexion_Log, Etudiant } = require('../bd');
 
 module.exports = (app) => {
     app.get('/api/log/connexion', verifyToken, verifyAdmin, async (req, res) => {
         try {
-            const logs = await Connexion_Log.findAll();
-            res.json(logs);
+            const logs = await Connexion_Log.findAll({
+                attributes: ['date'],
+                include: [
+                    {
+                        model: Etudiant,
+                        attributes: ['classe_id'],
+                    }
+                ],
+                order: [['date', 'DESC']],
+            });   
+            const formated_data = {
+                logs: logs.map(log => ({
+                    date: log.date,
+                    classe_id: log.Etudiant.classe_id
+                }))
+            }
+            res.json(formated_data); 
         } catch (error) {
             res.status(500).json({ error: 'An error occurred while fetching logs' });
         }
