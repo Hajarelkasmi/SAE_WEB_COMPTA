023802412ee3196780e0d29af4b0a8db75ea7b9b
Rousselet Juliette <juliette.rousselet@etu.univ-orleans.fr>
2024-10-25 14:31:17 +0200

    Refactor Footer component to update GitHub link and add navigation links in the footer

--- src/composants/Footer.js
+++ src/composants/Footer.js
@@ -12,17 +12,17 @@ function Footer() {
                 <a href="https://www.gmail.com" alt="gmail" target="_blank" rel="noreferrer"><img src="/logo_mail.png" alt="logo" className="logo" /></a>
             </div>
             <p>
-                <span>@Site réalisé par : </span><a href="https://www.github.com" target="_blank" rel="noreferrer">Brizard Amélie</a>, <a href="https://www.github.com" target="_blank" rel="noreferrer">El Kasmi Hajar</a>, <a href="https://www.github.com" target="_blank" rel="noreferrer">Haudebourg Baptiste</a>, <a href="https://www.github.com" target="_blank" rel="noreferrer">Merit Juliann</a>, <a href="https://www.github.com" target="_blank" rel="noreferrer">Rousselet Juliette</a>
+                <span>@Site réalisé par : </span><a href="https://github.com/amelie-brizard" target="_blank" rel="noreferrer">Brizard Amélie</a>, <a href="https://github.com/Hajarelkasmi" target="_blank" rel="noreferrer">El Kasmi Hajar</a>, <a href="https://github.com/bap-haudebourg" target="_blank" rel="noreferrer">Haudebourg Baptiste</a>, <a href="https://github.com/JuliannMerit" target="_blank" rel="noreferrer">Merit Juliann</a>, <a href="https://github.com/Juliette0070" target="_blank" rel="noreferrer">Rousselet Juliette</a>
             </p>
-            <p id="footer_github_logo">Voir notre travail : <a href="https://www.github.com/" target="_blank" rel="noreferrer"><img src="/github_logo.png" alt="github" className="logo" /></a></p>
+            <p id="footer_github_logo">Voir notre travail : <a href="https://github.com/Hajarelkasmi/SAE_WEB_COMPTA" target="_blank" rel="noreferrer"><img src="/github_logo.png" alt="github" className="logo" /></a></p>
         </div>
         <div id="footer_links">
             <p>
-                <a href="https://www.github.com" target="_blank" rel="noreferrer">Accessibilité</a>
-                <a href="https://www.github.com" target="_blank" rel="noreferrer">Nous contacter</a>
-                <a href="https://www.github.com" target="_blank" rel="noreferrer">Légal</a>
-                <a href="https://www.github.com" target="_blank" rel="noreferrer">Protection des données</a>
-                <a href="https://www.github.com" target="_blank" rel="noreferrer">Sécurité et confidentialité</a>
+                <a href="/accessibilite" rel="noreferrer">Accessibilité</a>
+                <a href="/contact" rel="noreferrer">Nous contacter</a>
+                <a href="/legal" rel="noreferrer">Légal</a>
+                <a href="/protection_donnees" rel="noreferrer">Protection des données</a>
+                <a href="/securite_confidentialite" rel="noreferrer">Sécurité et confidentialité</a>
             </p>
         </div>
     </footer>
