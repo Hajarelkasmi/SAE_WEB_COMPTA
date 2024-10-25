855a1873b2a81d830c9350f1ac50c0ae55de5ab8
Baptiste Haudebourg <baptiste.haudebourg@etu.univ-orleans.fr>
2024-10-25 10:50:41 +0200

    ajout de pouvoir rechercher par page_id classe_page.js

--- src/model/module/classe_page.js
+++ src/model/module/classe_page.js
@@ -6,6 +6,12 @@ const { verifyToken, verifyAdmin } = require('../auth');
 module.exports = (app) => {
     app.get('/api/classe_pages', async (req, res) => {
         try {
+            const { page_id } = req.query;
+            if (page_id) {
+                const classe_pages = await Classe_Page.findAll({ where: { page_id } });
+                res.json(classe_pages);
+                return;
+            }
             const classe_pages = await Classe_Page.findAll();
             res.json(classe_pages);
         } catch (error) {
@@ -27,7 +33,12 @@ module.exports = (app) => {
 
     app.delete('/api/classe_pages/:id_classe/:id_page', verifyToken, verifyAdmin, async (req, res) => {
         try {
-            const classe_page = await Classe_Page.findByPk(req.params.id_classe, req.params.id_page);
+            const classe_page = await Classe_Page.findOne({
+                where: {
+                    classe_id: req.params.id_classe,
+                    page_id: req.params.id_page
+                }
+            });
             if (classe_page) {
                 await classe_page.destroy();
                 res.json(classe_page);
