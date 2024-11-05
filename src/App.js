1164638a7f2961c505f4b2f54bfa9720a0a05a25
Rousselet Juliette <juliette.rousselet@etu.univ-orleans.fr>
2024-11-05 17:09:08 +0100

    Update 404 error message in App component for clearer user feedback

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ function App() {
                                     <Route path="/categories/:id_categorie/pages" element={<Create_Page/>}/>
 
                                     {/* Route par défaut */}
-                                    <Route path="*" element={<h1>Erreur 404</h1>}/>
+                                    <Route path="*" element={<h1>Erreur 404: Page non trouvée</h1>}/>
                                 </Routes>
                             </div>
                             <Footer/>
