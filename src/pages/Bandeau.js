39cc1687afd3b6c67cd131a595f74cb9169ff70e
Rousselet Juliette <juliette.rousselet@etu.univ-orleans.fr>
2024-11-20 17:08:09 +0100

    Refactor Bandeau component to handle admin status fetching with error handling; update authentication links and improve menu toggle functionality

--- src/pages/Bandeau.js
+++ src/pages/Bandeau.js
@@ -4,13 +4,17 @@ import ElemReseau from './ElemReseau';
 import { useEffect, useState } from 'react';
 import {checkAdmin} from "./CheckAdmin";
 
-function Bandeau({reseaux}) {
+function Bandeau() {
     const [data, setData] = useState([]);
     const [isAdmin, setIsAdmin] = useState(null);
     useEffect(() => {
         const fetchAdminStatus = async () => {
-            const adminStatus = await checkAdmin();
-            setIsAdmin(adminStatus);
+            try {
+                const adminStatus = await checkAdmin();
+                setIsAdmin(adminStatus);
+            } catch (error) {
+                console.error('Erreur:', error);
+            }
         };
 
         fetchAdminStatus();
@@ -35,43 +39,53 @@ function Bandeau({reseaux}) {
         fetchData();
     }, []);
     
-
-  window.onload = function() {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const header = document.querySelector('header');
-
-    menuToggle.addEventListener('click', function() {
-        header.classList.toggle('menu-open');
-        if (header.classList.contains('menu-open')) {
-            menuToggle.innerHTML = '✖';
+    const [auths, setAuths] = useState([]);
+    useEffect(() => {
+        if (localStorage.getItem('token')) {
+            setAuths([
+                {img: '/deconnexion.png', link: '/deconnexion'},
+                {img: '/compte.png', link: '/compte'}
+            ]);
         } else {
-            menuToggle.innerHTML = '☰';
+            setAuths([
+                {img: '/connexion.png', link: '/connexion'},
+                {img: '/inscription.png', link: '/inscription'}
+            ]);
         }
-    });
-  };
-  return (
-    <header style = {{ backgroundColor : isAdmin ? '#a63629' : '#1c3f59' }}>
-        <button className='menu-toggle'>☰</button>
-        <nav>
-            <a href='/' id="logohome"><img src="/logo_bitmoji.png" alt="logo" className='logo' /></a>
-            <ul id="pages">
-                <ElemBandeau link="/accueil" nom="Accueil" enfants={[]} />
-                {data.map((elem, index) => (
-                    <ElemBandeau key={index} link={"/categories/" + elem.id} nom={elem.nom} enfants={elem.enfants} />
-                ))}
-                <ElemBandeau link="/blog" nom="Blog" enfants={[]} />
-                { localStorage.getItem('token') ? <ElemBandeau link="/deconnexion" nom="Déconnexion" enfants={[]} /> : <ElemBandeau link="/connexion" nom="Connexion" enfants={[]} />
+    }, []);
 
-                }
-            </ul>
-            <ul id="reseaux">
-              {reseaux.map((elem, index) => (
-                    <ElemReseau key={index} img={elem.img} link={elem.link} />
-                ))}
-            </ul>
-        </nav>
-    </header>
-  );
+    window.onload = function() {
+        const menuToggle = document.querySelector('.menu-toggle');
+        const header = document.querySelector('header');
+
+        menuToggle.addEventListener('click', function() {
+            header.classList.toggle('menu-open');
+            if (header.classList.contains('menu-open')) {
+                menuToggle.innerHTML = '✖';
+            } else {
+                menuToggle.innerHTML = '☰';
+            }
+        });
+    };
+    return (
+        <header style = {{ backgroundColor : isAdmin ? '#a63629' : '#1c3f59' }}>
+            <button className='menu-toggle'>☰</button>
+            <nav>
+                <a href='/' id="logohome"><img src="/logo_bitmoji.png" alt="logo" className='logo' /></a>
+                <ul id="pages">
+                    {/* <ElemBandeau link="/accueil" nom="Accueil" enfants={[]} /> */}
+                    {data.map((elem) => (
+                        <ElemBandeau key={elem} link={"/categories/" + elem.id} nom={elem.nom} enfants={elem.enfants} />
+                    ))}
+                </ul>
+                <ul id="auths">
+                {auths.map((elem) => (
+                        <ElemReseau key={elem.id} img={elem.img} link={elem.link} />
+                    ))}
+                </ul>
+            </nav>
+        </header>
+    );
 }
 
 export default Bandeau;
