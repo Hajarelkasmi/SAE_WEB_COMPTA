dc80dfb04df854f4c7cab330cfba634d31ed67ce
Rousselet Juliette <juliette.rousselet@etu.univ-orleans.fr>
2024-11-27 16:09:23 +0100

    Refactor Footer component to dynamically set background color based on admin status

--- src/pages/Bandeau.js
+++ src/pages/Bandeau.js
@@ -38,10 +38,6 @@ function Bandeau() {
         fetchData();
     }, []);
 
-    // return (
-    //<header style={{ backgroundColor: isAdmin ? '#a63629' : '#1c3f59' }}>
-
-
     useEffect(() => {
         if (localStorage.getItem('token')) {
             setAuths([
