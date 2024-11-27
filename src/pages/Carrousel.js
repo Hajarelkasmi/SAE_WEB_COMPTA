df30f80d2d7b7c65ac3856ce312f58dfcd0f8ffe
Baptiste Haudebourg <baptiste.haudebourg@etu.univ-orleans.fr>
2024-11-27 16:27:53 +0100

    correction image caroussel

--- src/pages/Carrousel.js
+++ src/pages/Carrousel.js
@@ -164,7 +164,7 @@ function Carrousel() {
                     if (element.image === null) {
                         element.img = "/logo_bitmoji.png";
                     } else {
-                        element.img = element.image;
+                        element.img = "/static/image/"+element.image;
                     }
                 }
                 //  donner leur place depuis elemsCarrousel
