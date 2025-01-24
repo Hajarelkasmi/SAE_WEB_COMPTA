cf072051ca9036791b398f0f0bf53b50fa08f96c
Rousselet Juliette <juliette.rousselet@etu.univ-orleans.fr>
2025-01-24 17:55:03 +0100

    Mise à jour de la gestion de la position et de la taille des images dans le carrousel, ajout de la gestion du décalage lors du redimensionnement et amélioration de la prévisualisation de l'image.

--- src/pages/Create_Categorie.js
+++ src/pages/Create_Categorie.js
@@ -63,9 +63,11 @@ const Create_Categorie = () => {
                         const pos = JSON.parse(data.position);
                         setPosition({ x: pos.x, y: pos.y });
                     }
-                    if (data.positionCarrousel) {
-                        const pos = JSON.parse(data.positionCarrousel);
+                    if (data.placement_image_carrousel) {
+                        const pos = JSON.parse(data.placement_image_carrousel);
                         setPositionC({ xC: pos.x, yC: pos.y });
+                        setSizeC({ width: pos.width, height: pos.height });
+                        setResizeOffset({ deltaX: pos.decX, deltaY: pos.decY });
                     }
                 } catch (error) {
                     console.error('Erreur:', error);
@@ -87,7 +89,7 @@ const Create_Categorie = () => {
             formData.append('description', description);
             formData.append('est_public', estPublic);
             formData.append('image', imageFile);
-            formData.append('placement_image_carrousel', JSON.stringify({ x: positionC.xC + resizeOffset.deltaX, y: positionC.yC + resizeOffset.deltaY, width: sizeC.width, height: sizeC.height }));
+            formData.append('placement_image_carrousel', JSON.stringify({ x: positionC.xC + resizeOffset.deltaX, y: positionC.yC + resizeOffset.deltaY, width: sizeC.width, height: sizeC.height, decX: resizeOffset.deltaX, decY: resizeOffset.deltaY }));
 
             const response = await fetch(url, {
                 method: method,
@@ -145,36 +147,38 @@ const Create_Categorie = () => {
                 </Draggable>
 
             </div>
-            <p id="prev_img_carrousel_titre">Prévisualisation de l'image dans le carrousel</p>
-            <div id="cont-carrousel">
-                <div id="square-container">
-                    <div id="circle-container"></div>
-                    <Draggable onDrag={handleDragCarrousel} cancel=".react-resizable-handle"> 
-                        <ResizableBox
-                            width={sizeC.width}
-                            height={sizeC.height}
-                            minConstraints={[100, 100]} // Dimensions minimales
-                            maxConstraints={[10000, 10000]} // Dimensions maximales
-                            resizeHandles={["se", "sw", "ne", "nw"]} // Poignées de redimensionnement
-                            onResize={handleResize}
-                        >
-                            <img
-                                src={image}
-                                alt="Aperçu de l'image"
-                                style={{
-                                    top: positionC.yC,
-                                    left: positionC.xC,
-                                    width: `${sizeC.width}px`,
-                                    height: `${sizeC.height}px`,
-                                    objectFit: "fill",
-                                    cursor: "grab",
-                                }}
-                                draggable="false"
-                            />
-                        </ResizableBox>
-                    </Draggable>
-                </div>
-            </div>
+            {image ? (
+            <><p id="prev_img_carrousel_titre">Prévisualisation de l'image dans le carrousel</p><div id="cont-carrousel">
+                    <div id="square-container">
+                        <div id="circle-container"></div>
+                        <Draggable onDrag={handleDragCarrousel} cancel=".react-resizable-handle" defaultPosition={{ x: positionC.xC-resizeOffset.deltaX, y: positionC.yC-resizeOffset.deltaY }}>
+                            <ResizableBox
+                                width={sizeC.width}
+                                height={sizeC.height}
+                                minConstraints={[100, 100]} // Dimensions minimales
+                                maxConstraints={[10000, 10000]} // Dimensions maximales
+                                resizeHandles={["se", "sw", "ne", "nw"]} // Poignées de redimensionnement
+                                onResize={handleResize}
+                            >
+                                <img
+                                    src={image}
+                                    alt="Aperçu de l'image"
+                                    style={{
+                                        top: positionC.yC,
+                                        left: positionC.xC,
+                                        width: `${sizeC.width}px`,
+                                        height: `${sizeC.height}px`,
+                                        objectFit: "fill",
+                                        cursor: "grab",
+                                    }}
+                                    draggable="false" />
+                            </ResizableBox>
+                        </Draggable>
+                    </div>
+                </div></>
+                    ) : (
+                <p>Ajoutez une image pour voir la prévisualisation</p>
+            )}
 
             <h1 className="title-create-cat">{categorieId ? 'Modifier' : 'Créer'} une catégorie</h1>
             <form className="form-create-cat" onSubmit={handleSubmit}>
